100ac1bb620f0668286e5eda81d75184ab5135f8
Pipat Saengow <pay2630@gmail.com>
2021-12-16 16:19:03 +0700

    use the word line instead of addr to prevent confusion

--- memory.v
+++ memory.v
@@ -1,20 +1,30 @@
 module memory(
-	input wire [11:0] addr,
+	input wire [11:0] line,
 	input wire [31:0] write_data,
 	input wire write,
 	input wire clk,
 	output wire [31:0] data,
-	input wire [11:0] inst_addr,
+	input wire [11:0] inst_line,
 	output wire [31:0] inst_data);
 
 reg [31:0] mem[0:4095];
-assign data = mem[addr];
-assign inst_data = mem[inst_addr];
+assign data = mem[line];
+assign inst_data = mem[inst_line];
+
+parameter ROM_FILE = "NONE";
 
 always @(posedge clk) begin
 	if (write) begin
-		mem[addr] <= write_data;
+		mem[line] <= write_data;
 	end
+end
+
+initial begin
+	if (ROM_FILE != "NONE") begin
+		$display("MEM LOADED");
+		$readmemh("rom.txt", mem);
+	end else
+		$display("NO MEM");
 end
 
 endmodule
--- memory_tb.v
+++ memory_tb.v
@@ -2,37 +2,37 @@
 
 module memory_tb;
 
-reg [31:0] addr, write_data, inst_addr;
+reg [31:0] line, write_data, inst_line;
 reg write, clk;
 
 wire [31:0] data, inst_data;
 
-memory m(.addr(addr[11:0]), .write_data(write_data), .write(write), .clk(clk), .data(data), .inst_addr(inst_addr[11:0]), .inst_data(inst_data));
+memory m(.line(line[11:0]), .write_data(write_data), .write(write), .clk(clk), .data(data), .inst_line(inst_line[11:0]), .inst_data(inst_data));
 
 always #5 clk = ~clk;
 
 initial begin
 clk = 0;
 write = 1;
-addr = 0;
+line = 0;
 write_data = 'hFF;
 
 #10 $display("Written 0 = FF");
 
-addr = 1;
+line = 1;
 write_data = 'hBB;
 
 #10 $display("Writted 1 = BB");
 
 write = 0;
-addr = 0;
+line = 0;
 
 #10 $display("Read back: %d", data == 'hFF);
 
-addr = 1;
+line = 1;
 #10 $display("Read back: %d", data == 'hBB);
 
-inst_addr = 1;
+inst_line = 1;
 #10 $display("Read back: %d", data == 'hBB);
 
 
